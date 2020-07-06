f2042588cf24586c431165f836747767f6a61ca4
austin <ap.hugus@gmail.com>
2020-07-06 16:33:16 -0600

    working

--- Program.cs
+++ Program.cs
@@ -11,10 +11,10 @@ namespace rps {
             string name = Console.ReadLine ();
             bool valid = false;
             string ans = "";
-            while (!valid) {
 
-                Console.WriteLine ($"Hello {name}, Are you Ready To Rock The Paper, Paper The Scissor and Scissor the Paper??");
-                Console.WriteLine ("Are You Sure You Wish To Enter This Tournament?  (Y/N)");
+            Console.WriteLine ($"Hello {name}, Are you Ready To Rock The Paper, Paper The Scissor and Scissor the Paper??");
+            Console.WriteLine ("Are You Sure You Wish To Enter This Tournament?  (Y/N)");
+            while (!valid) {
                 char choice = Console.ReadKey ().KeyChar;
                 Console.Clear ();
                 Dictionary<string, int> Weapons = new Dictionary<string, int> () { { "Rock", 1 }, { "Paper", 2 }, { "Scissors", 3 },
@@ -29,33 +29,30 @@ namespace rps {
     _______
 -- - '   ____)
       (_____)
-      (_____)
+      (_____)                       ROCK
       (____)
 -- -.__(___)
 
     _______
 -- - '   ____)
           ______)
-          _______)
+          _______)                  PAPER
          _______)
 ---.__________)
 
     _________
 -- - '   ____)___
-        _________)
+        _________)                  SCISSORS
        __________)
       (____)
 -- -.__(___)
 
                     ");
-
                     Console.WriteLine ("Choose Your Weapon! ROCK Paper Scissors");
-                    string[] choices = new string[3] { "ROCK", "PAPER", "SCISSORS" };
-                    string userchoice = Console.ReadLine ();
+                    string userChoice = Console.ReadLine ().ToUpper ();
                     Random r = new Random ();
                     int computerChoice = r.Next (0, 4);
-                    Console.WriteLine ("Enter Your Choice: ");
-                    string userChoice = Console.ReadLine ().ToUpper ();
+                    string[] choices = new string[3] { "ROCK", "PAPER", "SCISSORS" };
                     Console.WriteLine ("Computer:" + choices[computerChoice]);
 
                     if (userChoice == "ROCK" && choices[computerChoice] == "SCISSORS") {
@@ -68,10 +65,14 @@ namespace rps {
                         Console.WriteLine ("YOU SUCK LOSER!");
                     } else if (userChoice == "SCISSORS" && choices[computerChoice] == "ROCK") {
                         Console.WriteLine ("YOU SUCK LOSER!");
-                    } else if (userChoice == "SCISSORS" && choices[computerChoice] == "") {
-
+                    } else if (userChoice == "SCISSORS" && choices[computerChoice] == "PAPER") {
+                        Console.WriteLine ("YOU Win!");
+                    } else {
+                        Console.WriteLine ("It's a Tie! Try Again you sister kisser!");
                     }
-
+                    Console.WriteLine ("Play Again If You Dare? (Y/N)");
+                } else {
+                    Console.WriteLine ("SO LONG LOSER!");
                 }
             }
         }
